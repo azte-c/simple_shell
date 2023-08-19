7992918c8bdb6d3da25a91a04e56852f1ad26756
SharonObange <obangesharon@gmail.com>
2023-08-19 14:28:36 -0700

    get_line function added

--- shell.h
+++ shell.h
@@ -3,8 +3,8 @@
 /* the ssize_t type an error message*/
 void capture_input(char **prompt_user, char **user_input, ssize_t *input_bytes);
 void execute_command(char *user_input);
-
-
+ssize_t get_line(char *captured_input, size_t buffer_size,
+		char **line_ptr, size_t *n);
 
 
 #endif
