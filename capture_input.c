939d1b93b3294ce370a8e0879bdb36af2d0f93b1
SharonObange <obangesharon@gmail.com>
2023-08-22 09:43:52 -0700

    Exit built in implemented. Updated in capture_input.c

--- capture_input.c
+++ capture_input.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,6 +7,19 @@
 #include "shell.h"
 
 #define BUFFER_SIZE 1024
+
+/**
+  *exit_shell - exits the shell
+*/
+
+void exit_shell(void)
+{
+	char exit_output[] = "Exiting the shell ... \n";
+
+	write(STDOUT_FILENO, exit_output, sizeof(exit_outpu    t) - 1);
+	exit(0);
+}
+
 /**
  *capture_input - prompts the user and captures the input
  *@user_input_ptr:pointer to the user input
@@ -13,7 +27,7 @@
  */
 void capture_input(char **user_input_ptr, ssize_t *input_bytes_ptr)
 {
-	/*promp_user will store what to display to user*/ 
+	/*promp_user will store what to display to user*/
 	char *prompt_user, *user_input;/*user_input will hold input from user*/
 	ssize_t input_bytes;/* store the number of bytes read*/
 
@@ -21,19 +35,14 @@ void capture_input(char **user_input_ptr, ssize_t *input_bytes_ptr)
 	prompt_user[0] = '#';
 	prompt_user[1] = '\0';
 
-	/*writes out the prompt to be displayed*/
 	write(STDOUT_FILENO, prompt_user, 1);
-
-	/*dynamically allocate memory to user input to hold the users input*/
 	user_input = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 
-	/*this checks if the memory was allocated sucessfully*/
 	if (user_input == NULL)
 	{
 		perror("malloc");
 		exit(-1);
 	}
-	/*the read function reads input from the keyboard into user input*/
 	input_bytes = read(STDIN_FILENO, user_input, BUFFER_SIZE);
 
 	if (input_bytes == 0)
@@ -51,6 +60,9 @@ void capture_input(char **user_input_ptr, ssize_t *input_bytes_ptr)
 		exit(-1);
 	}
 	user_input[input_bytes - 1] = '\0';
+
+	if (strcmp(user_input, "exit") == 0)
+		exit_shell();
 	free(prompt_user);
 
 	*user_input_ptr = user_input;
