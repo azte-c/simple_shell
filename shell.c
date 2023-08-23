6ca63c7a3312e636535eadb31d448e1b5ad2af75
SharonObange <obangesharon@gmail.com>
2023-08-23 08:15:20 -0700

    Restored shell.c to answer task 1

--- shell.c
+++ shell.c
@@ -21,10 +21,11 @@ int main(void)
 		/*size_t line_size =0;*/
 		/*ssize_t line_len*/
 		ssize_t input_bytes;
-		int args_count, x;
-		char *argsv[BUFFER_SIZE / 2 + 1]; /*array to store arguments*/
+		/*int args_count, x;*/
+		/*char *argsv[BUFFER_SIZE / 2 + 1]; array to store arguments*/
 
 		capture_input(&user_input, &input_bytes);
+		execute_command(user_input);
 
 		/*line_len = get_line(user_input, input_bytes, &line, &line_size);*/
 
@@ -32,17 +33,16 @@ int main(void)
 		/*printf("Line read: %s\n", line);*/
 		/*printf("Line length: %ld\n", line_len);*/
 
-		tokenize_input(user_input, argsv, &args_count);
-		if (args_count > 0)
-		{
-			printf("Number of arguments: %d\n", args_count);
-			for (x = 0; x < args_count; x++)
-			{
-				printf("argsv[%d]; %s\n", x, argsv[x]);
-			}
+		/*tokenize_input(user_input, argsv, &args_count);*/
+		/*if (args_count > 0)*/
+		/*{*/
+			/*printf("Number of arguments: %d\n", args_count);*/
+			/*for (x = 0; x < args_count; x++)*/
+			/*{*/
+				/*printf("argsv[%d]; %s\n", x, argsv[x]);*/
+			/*}*/
 			/*get_path(argsv, &args_count);*/
-		}
-		/*execute_command(user_input);*/
+		/*}*/
 		free(user_input);
 		/*free(line);*/
 	}
